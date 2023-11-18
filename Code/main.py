d4adcf219d51fdda7ae0cf1a83427dc6c33c3729
L Dettling <tdettling@tds.net>
2023-11-18 08:42:17 -0500

    wip: geo file reading
    Signed-off-by: L Dettling <tdettling@tds.net>

--- Code/main.py
+++ Code/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import geoData 
 
 #file_name = "/Users/lauryndavis/"
-file_name = "/Users/Ldettling/Desktop"
+file_name = "/Users/Ldettling/Documents"
 
 
 # main file for running defs
@@ -13,15 +13,15 @@ file_name = "/Users/Ldettling/Desktop"
 #crimeDF = mac.readXLSXfilkes()
 #print(crimeDF)
 
-#crime_data = mac.fileReader(directory = "/Users/Ldettling/Desktop/CIS635")
-#print(crime_data)
+crime_data = mac.fileReader(directory = "/Users/Ldettling/Documents/CIS635")
+print(crime_data)
 
 #testdf = mac.smallTesterFile(file_name="/Users/Ldettling/Desktop")
 #print(testdf)
 #mac.nanColumns(testdf)
 
 #crimeData_geo = 
-geoData.shpFile_brute_MAC(file_name)
+#geoData.shpFile_brute_MAC(file_name)
 #print(crimeData_geo)
 
 # the above code is commented out for processing
