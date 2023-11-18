2af38f23e1b3aefb08c008ab48b397c13025a77f
L Dettling <46734576+tdettling@users.noreply.github.com>
2023-11-18 08:03:40 -0500

    C: xlsx reading, WIP: geo file reading

--- Code/main.py
+++ Code/main.py
@@ -1,8 +1,27 @@
 # imports
 import xlsxData
+import Macxlsxdata as mac
 import pandas as pd
+import geoData 
+
+#file_name = "/Users/lauryndavis/"
+file_name = "/Users/Ldettling/Desktop"
+
 
 # main file for running defs
-crimeDF = xlsxData.readXLSXfilkes()
-print(crimeDF)
+#crimeDF = xlsxData.readXLSXfilkes()
+#crimeDF = mac.readXLSXfilkes()
+#print(crimeDF)
 
+#crime_data = mac.fileReader(directory = "/Users/Ldettling/Desktop/CIS635")
+#print(crime_data)
+
+#testdf = mac.smallTesterFile(file_name="/Users/Ldettling/Desktop")
+#print(testdf)
+#mac.nanColumns(testdf)
+
+#crimeData_geo = 
+geoData.shpFile_brute_MAC(file_name)
+#print(crimeData_geo)
+
+# the above code is commented out for processing
