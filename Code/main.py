f3b6d441fe4f3423b58174b9e97396b4592baadf
L Dettling <tdettling@tds.net>
2023-11-18 09:06:41 -0500

    wip: main def running
    Signed-off-by: L Dettling <tdettling@tds.net>

--- Code/main.py
+++ Code/main.py
@@ -7,21 +7,18 @@ import geoData
 #file_name = "/Users/lauryndavis/"
 file_name = "/Users/Ldettling/Documents"
 
+def mainRun(runType = 'Main', readGeo = False):
+    if runType == 'Test':
+        testdf = mac.smallTesterFile(file_name="/Users/Ldettling/Desktop")
+        print(testdf)
+        mac.nanColumns(testdf)
+    else:
+        crime_data = mac.fileReader(directory = "/Users/Ldettling/Documents/CIS635")
+        print(crime_data)
 
-# main file for running defs
-#crimeDF = xlsxData.readXLSXfilkes()
-#crimeDF = mac.readXLSXfilkes()
-#print(crimeDF)
+        if readGeo:
+            crimeData_geo = geoData.shpFile_brute_MAC(file_name)
+            print(crimeData_geo)
 
-crime_data = mac.fileReader(directory = "/Users/Ldettling/Documents/CIS635")
-print(crime_data)
 
-#testdf = mac.smallTesterFile(file_name="/Users/Ldettling/Desktop")
-#print(testdf)
-#mac.nanColumns(testdf)
-
-#crimeData_geo = 
-#geoData.shpFile_brute_MAC(file_name)
-#print(crimeData_geo)
-
-# the above code is commented out for processing
+mainRun()
