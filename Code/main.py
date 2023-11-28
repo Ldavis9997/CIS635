9e9b142f05f30d19cb8a6aaea5b23c22db236949
L Dettling <tdettling@tds.net>
2023-11-28 18:11:27 -0500

    wip: uploading 2012 dbf

--- Code/main.py
+++ Code/main.py
@@ -1,26 +1,3 @@
-# imports
-import xlsxData
-import Macxlsxdata as mac
-import pandas as pd
-import geoData 
-
-file_name = "/Users/lauryndavis/"
-#file_name = "/Users/Ldettling/Documents"
-data_directory = "/Users/Ldettling/Documents/CIS635"
-geo_directory = "/Users/Ldettling/Documents/CIS635/Geo_Data"
-
-def mainRun(runType = '', readGeo = False):
-    if runType == 'Test':
-        testdf = mac.smallTesterFile(file_name)
-        print(testdf)
-        mac.nanColumns(testdf)
-    elif runType == 'Main':
-        crime_data = mac.fileReader(data_directory)
-        print(crime_data)
-
-    if readGeo:
-        crimeData_geo = geoData.shpFile_brute_MAC(file_name)
-        #crimeData_geo = geoData.shpFileReader(geo_directory)
-        print(crimeData_geo)
-
-mainRun('Main', readGeo=False)
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c9bc45253966ba0db4b79f3fa88ff83b0d90bc7eb385f8258f822c50cf4e070
+size 758
